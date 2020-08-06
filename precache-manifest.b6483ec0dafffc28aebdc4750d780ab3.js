505b6766f4097f8df58c6b9c4329ccf0498e31aa
cazdemun <cplu1509@gmail.com>
2020-08-06 01:19:52 -0500

    Updates

---
+++ precache-manifest.b6483ec0dafffc28aebdc4750d780ab3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7468e611558308d83192838e34a7d28f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "370899bf992422bb63d0",
+    "url": "/static/css/main.f8c32209.chunk.css"
+  },
+  {
+    "revision": "f06ab08c794f27e6f97c",
+    "url": "/static/js/2.9d663282.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.9d663282.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "370899bf992422bb63d0",
+    "url": "/static/js/main.40cdfd62.chunk.js"
+  },
+  {
+    "revision": "bd83e326b30b30fbf950",
+    "url": "/static/js/runtime-main.2bcf68e0.js"
+  }
+]);
